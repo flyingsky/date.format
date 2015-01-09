43af5b1bdd6403ecdc10fb4aa4fe6f9c9050b580
Ramon Liu <llmfei@gmail.com>
2015-01-08 16:25:34 -0800

    add mysql ADDTIME function into JS Date

--- date.format.js
+++ date.format.js
@@ -82,4 +82,128 @@
       return (esc === '' && replaceChars[chr]) ? replaceChars[chr].call(date) : chr;
     });
   };
+
+  function _isTimeStr(timeStr) {
+    return timeStr.trim().indexOf('-') <= 0; // maybe negative timeStr, so index 0 is allowed
+  }
+
+  /**
+   * @param timeStr {String} like '24:10:10', '1:1:1.12', hour:minute:second
+   * @return {Number} in mill seconds
+   */
+  function _parseTimeStr(timeStr) {
+    if (timeStr.indexOf(':') <= 0) { // only for seconds
+      return parseFloat(timeStr * 1000);
+    }
+
+    var hms = timeStr.split(':');
+    var result = 0;
+    var len = 3;
+    for (var i = 0; i < len; i++) { // h:m:s
+      var value = hms[i] || 0;
+      result = result * 60 + ((i === len - 1) ? parseFloat(value) : parseInt(value)); // only second part maybe float number
+    }
+
+    if (hms.length < len && timeStr.lastIndexOf('.') > 0) { // 2:2.1 means 2 hours 2 minutes and 0.1 seconds
+      result += parseFloat(timeStr.substr(timeStr.lastIndexOf('.')));
+    }
+      
+    return result * 1000;
+  }
+
+  /**
+   * @param timeStr {String} like '1 1:1:1', '-2 2:2' day hour:minute:second
+   */
+  function _parseTimeExpr(timeExpr) {
+    timeExpr = timeExpr.trim();
+    var sign = timeExpr[0] === '-' ? -1 : 1;
+    if (sign === -1) {
+      timeExpr = timeExpr.substr(1);
+    }
+    
+    var dayAndTime = timeExpr.trim().split(/\s+/);
+    if (dayAndTime.length > 2) {
+      return 0;                 // invalid timeStr
+    }
+
+    var day = 0;
+    var timeStr = 0;
+    if (dayAndTime.length === 2) {
+      day = parseInt(dayAndTime[0]);
+      timeStr = dayAndTime[1];
+    } else {
+      timeStr = dayAndTime[0];
+    }
+
+    var DAY_TO_MILLISECONDS = 24 * 3600 * 1000;
+
+    return (day * DAY_TO_MILLISECONDS + _parseTimeStr(timeStr)) * sign;
+  }
+
+  function _paddingZeroRight(number, len) {
+    var str = number.toString();
+    if (str.length < len) {
+      str = '0' + str;
+    }
+    return str;
+  }
+  
+  /**
+   * @param milliseconds {Number}
+   * @return {String} such as '27:30:29.234'
+   */
+  function _formatToTimeExpr(milliseconds) {
+    var str = milliseconds.toString();
+    var dotPart = '';
+    var dotIndex = str.indexOf('.');
+    if (dotIndex > 0) {
+      dotPart = str.substr(dotIndex + 1);
+      str = str.substr(0, dotIndex);
+    }
+    
+    milliseconds = parseInt(str);
+    var milli = milliseconds % 1000;
+    var seconds = (milliseconds - milli) / 1000;
+    var minutes = (seconds - seconds % 60) / 60;
+    seconds = seconds % 60;
+    var hours = (minutes - minutes % 60) / 60;
+    minutes = minutes % 60;
+
+    var result = [_paddingZeroRight(hours, 2), _paddingZeroRight(minutes, 2), _paddingZeroRight(seconds, 2)].join(':');
+    var milliStr = _paddingZeroRight(milli, 3) + dotPart;
+    if (milliStr.replace(/0/g, '').length === 0) { // all 0
+      milliStr = '';
+    } else {
+      milliStr = '.' + milliStr;
+    }
+    
+    return result + milliStr;
+  }
+
+  Date.prototype.addTime = function(milliseconds) {
+    var d = this;
+    
+    if (typeof(milliseconds) === 'string') { // time expr
+      milliseconds = _parseTimeExpr(milliseconds);
+    }
+    
+    d.setTime(d.getTime() + milliseconds);
+  };
+  
+  /**
+   * Same to Mysql ADDTIME function
+   * @param timeExpr1 {String} Date string or time string
+   * @param timeExpr2 {String} time string
+   * @return {String} Date string or time string
+   */
+  Date.addTime = function(timeExpr1, timeExpr2) {
+    if (_isTimeStr(timeExpr1)) {
+      var milliseconds = _parseTimeExpr(timeExpr1) + _parseTimeExpr(timeExpr2);
+      return _formatToTimeExpr(milliseconds);
+    } else {
+      var d = new Date(timeExpr1); // how to handle time1 is not date
+      d.addTime(timeExpr2);
+      return d.format('Y-m-d H:i:s.u');
+    }
+  };
 }).call(this);
